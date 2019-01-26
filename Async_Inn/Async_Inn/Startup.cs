7c9c89ef33a143a941cb78b255eb94622491155f
ntibbals <ntibbals@gmail.com>
2019-01-25 16:27:59 -0800

    setup starter server

--- Async_Inn/Async_Inn/Startup.cs
+++ Async_Inn/Async_Inn/Startup.cs
@@ -11,6 +11,7 @@ namespace Async_Inn
 {
     public class Startup
     {
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -25,9 +26,14 @@ namespace Async_Inn
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            /// static files///
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=home}/{action=Index}/{id?}");
             });
         }
     }
