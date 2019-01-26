e5348461e950201b211303fd093906277b11d3ef
ntibbals <ntibbals@gmail.com>
2019-01-25 17:22:19 -0800

    setup classes, tables and hotel

---
+++ Async_Inn/Async_Inn/Models/Hotel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Models
+{
+    public class Hotel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Navigation properties
+        /// </summary>
+        public HotelRoom HotelRoom { get; set; }
+    }
+}
