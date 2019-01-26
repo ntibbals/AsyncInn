d6fcd8124dd1b67ac65613cccd2d9e80f029573a
ntibbals <ntibbals@gmail.com>
2019-01-25 16:29:45 -0800

    setup home controller

---
+++ Async_Inn/Async_Inn/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
