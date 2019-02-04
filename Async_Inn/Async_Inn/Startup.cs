4c0d8c1b2059717fdd19e3bd9be387edcf7c3713
ntibbals <ntibbals@gmail.com>
2019-02-03 22:36:27 -0800

    re-routed to azure deployement

--- Async_Inn/Async_Inn/Startup.cs
+++ Async_Inn/Async_Inn/Startup.cs
@@ -30,7 +30,7 @@ namespace Async_Inn
 
             services.AddMvc();
             services.AddDbContext<AsyncInnDbContext>(options => 
-            options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+            options.UseSqlServer(Configuration["ConnectionStrings:ProductionConnection"]));
 
             services.AddScoped<IAmenitiesManager, AmenitiesManagementServices>(); /// registering interfaces
             services.AddScoped<IHotelManager, HotelManagementServices>();
